45832cec068e6d235c77a0be53adc02a388e0ab6
Thales Gomes <thalessud@gmail.com>
2021-03-24 21:44:04 +0000

    seeds refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,46 @@
 flats = [
   {
     name: 'Charm at the Steps of the Sacre Coeur/Montmartre',
-    imageUrl: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg',
+    image_url: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg',
     price: 164,
     lat: 48.884211,
     lng: 2.34689
   },
   {
     name: 'Trendy Apt in Buttes Montmartre',
-    imageUrl: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg',
+    image_url: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg',
     price: 200,
     lat: 48.885707,
     lng: 2.343543
   },
   {
     name: 'Super 60m2 in trendy neighborhood!',
-    imageUrl: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg',
+    image_url: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg',
     price: 150,
     lat: 48.885312,
     lng: 2.341225
   },
   {
     name: 'Splendide terrasse vue imprenable',
-    imageUrl: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat4.jpg',
+    image_url: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat4.jpg',
     price: 115,
     lat: 48.88184,
     lng: 2.343371
   },
   {
     name: 'Superbe vue à 2 min du Sacré Coeur',
-    imageUrl: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat5.jpg',
+    image_url: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat5.jpg',
     price: 135,
     lat: 48.888839,
     lng: 2.339208
   },
   {
     name: 'Bohemian and Chic in Paris',
-    imageUrl: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat6.jpg',
+    image_url: 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat6.jpg',
     price: 90,
     lat: 48.827855,
     lng: 2.350774
   }
 ]
 
-flats.each do |flat|
-  Flat.create!(price: flat[:price], lat: flat[:lat], lng: flat[:lng], name: flat[:name], image_url: flat[:imageUrl])
-end
+flats.each { |flat| Flat.create!(flat) }
